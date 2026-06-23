f229ddc30f1c4703a5576bb42fc32a0e7ed3b4e1
prabhutm <54661671+prabhutm@users.noreply.github.com>
2026-06-23 15:29:22 +0530

    Shapes Test Cases

---
+++ JavaTest1.java
@@ -0,0 +1,12 @@
+Point p1 = new Point(0, 0);
+Point p2 = new Point(4, 0);
+Point p3 = new Point(4, 3);
+Point p4 = new Point(0, 3);
+
+Trapezoid trapezoid = new Trapezoid(p1, p2, p3, p4);
+Rectangle rectangle = new Rectangle(p1, p2, p3, p4);
+Square square = new Square(p1, 3);
+
+Area of trapezoid=12.0
+Area of rectangle=12.0
+Area of square=9.0
